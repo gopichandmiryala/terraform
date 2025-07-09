dceae4d7357fffb52a09ef4e9a9a88c74c4df4bf
gopichandmiryala <gopichandmiryala159@outlook.com>
2025-07-09 23:25:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "local_file" "my_pet" {
-filename = var.filename
-content = "my pet name is ${random_pet.petname.id}"
-
-}
 resource "random_pet" "petname" {
-prefix = "MRR"
-separator = "."
-length = "2"
+  prefix    = "MRR"
+  separator = "."
+  length    = 2
+}
+
+resource "local_file" "my_pet" {
+  filename = "${path.module}/pet.txt"
+  content  = "my pet name is husky"
 }
